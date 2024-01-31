666c5035a07ac37663d81f2680327f4d8835ee44
varsha <01fe22bca019@kletech.ac.in>
2024-01-31 21:37:43 +0530

    Prime number

---
+++ prime1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class prime1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        
+        scanner.close();
+    }
+
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
